af02c340fa5e457032c61002a6e797cfbd50e28b
siddique <siddiquemd161@gmail.com>
2024-10-21 16:06:22 +0600

    next

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
+      external: [],  // Or just remove this line entirely if not needed
     },
   },
 });
